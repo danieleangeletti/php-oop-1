0be69b36b55d02c1615a62a0b021e59237c76e77
danieleangeletti <angelettidaniele@gmail.com>
2024-02-16 16:59:19 +0100

    bonus 2: layout con table

--- index.php
+++ index.php
@@ -30,7 +30,41 @@ class Movie
     {
         $this->genres[] = $genre;
     }
+    public function get_title()
+    {
+        return $this->title;
+    }
+    public function get_original_language()
+    {
+        return $this->original_language;
+    }
+    public function get_execution_time()
+    {
+        return $this->execution_time;
+    }
+    public function get_producer()
+    {
+        return $this->producer;
+    }
+    public function get_country_of_origin()
+    {
+        return $this->country_of_origin;
+    }
+    public function get_genres()
+    {
+        $genres_list = '';
+        foreach ($this->genres as $genre) {
+            $genres_list .= $genre . ' ';
+        }
+        return $genres_list;
+    }
+
 }
+$scream_VI_genres = array('horror', 'suspense', 'slasher');
+$scream_VI = new Movie('Scream VI', 'English', '1h 57m', 'Matt Bettinelli-Olpin, Tyler Gillett', 'USA', $scream_VI_genres);
+
+$requiem_for_a_dream_genres = array('horror', 'thriller', 'dramatic');
+$requiem_for_a_dream = new Movie('Requiem for a Dream', 'English', '1h 37m', 'Darren Aronofsky', 'Poland', $requiem_for_a_dream_genres);
 ?>
 
 
@@ -58,21 +92,64 @@ class Movie
 
     <main>
         <div class="container">
-            <?php
-            $scream_VI_genres = array('horror', 'suspense', 'slasher');
-            $scream_VI = new Movie('Scream VI', 'English', '1h 57m', 'Matt Bettinelli-Olpin, Tyler Gillett', 'USA', $scream_VI_genres);
-            echo $scream_VI->get_details();
-            ?>
-            <br>
-            <?php
-            $requiem_for_a_dream_genres = array('horror', 'thriller', 'dramatic');
-            $requiem_for_a_dream = new Movie('Requiem for a Dream', 'English', '1h 37m', 'Darren Aronofsky', 'Poland', $requiem_for_a_dream_genres);
-            echo $requiem_for_a_dream->get_details();
-            ?>
-
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Original language</th>
+                        <th scope="col">Execution time</th>
+                        <th scope="col">Producer</th>
+                        <th scope="col">Country of origin</th>
+                        <th scope="col">Genres</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <?php echo $scream_VI->get_title() ?>
+                        </td>
+                        <td>
+                            <?php echo $scream_VI->get_original_language() ?>
+                        </td>
+                        <td>
+                            <?php echo $scream_VI->get_execution_time() ?>
+                        </td>
+                        <td>
+                            <?php echo $scream_VI->get_producer() ?>
+                        </td>
+                        <td>
+                            <?php echo $scream_VI->get_country_of_origin() ?>
+                        </td>
+                        <td>
+                            <?php echo $scream_VI->get_genres() ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php echo $requiem_for_a_dream->get_title() ?>
+                        </td>
+                        <td>
+                            <?php echo $requiem_for_a_dream->get_original_language() ?>
+                        </td>
+                        <td>
+                            <?php echo $requiem_for_a_dream->get_execution_time() ?>
+                        </td>
+                        <td>
+                            <?php echo $requiem_for_a_dream->get_producer() ?>
+                        </td>
+                        <td>
+                            <?php echo $requiem_for_a_dream->get_country_of_origin() ?>
+                        </td>
+                        <td>
+                            <?php echo $requiem_for_a_dream->get_genres() ?>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </main>
 
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script type="text/javascript" src="./javascript/scripts.js"></script>
 </body>
 
